54021d79fd1a9c712c9c0c81d3a65016eaa6e7f9
kzubair007 <id.zubairkhan@gmail.com>
2020-02-26 22:18:18 +0530

    sampleApp

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void sendNow(View view){
+        Toast.makeText(this, "sending", Toast.LENGTH_SHORT).show();
+    }
+
+    public void recieveNow(View view) {
+        Toast.makeText(this, "recieveing", Toast.LENGTH_SHORT).show();
+    }
+    public void deleteNow(View view) {
+        Toast.makeText(this, "deleting", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
